744e434cfdf5ea8e3a6d10cf9dfb55bf05947f3b
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-12-01 17:40:32 +0530

    added repository annotation on video repository

--- vimo_backend/src/main/java/com/avaneesh/vimo_backend/common/repositories/VideoRepository.java
+++ vimo_backend/src/main/java/com/avaneesh/vimo_backend/common/repositories/VideoRepository.java
@@ -2,9 +2,11 @@ package com.avaneesh.vimo_backend.common.repositories;
 
 import com.avaneesh.vimo_backend.common.entities.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
 
     List<Video> findByTitleContainingIgnoreCase(String title);
