65a534e0c3f489a111f3d76182f6befed6fd3c4d
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-12-02 10:14:28 +0530

    style: replaced radix ui for react spectrum

--- vimo_frontend/apps/web/src/main.tsx
+++ vimo_frontend/apps/web/src/main.tsx
@@ -1,16 +1,37 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useHref,
+  useNavigate,
+  type NavigateOptions,
+} from "react-router";
+import { defaultTheme, Provider } from "@adobe/react-spectrum";
+
 import "./style.css";
-
 import HomePage from "./pages/Home";
+import UploadPage from "./pages/Upload";
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  </BrowserRouter>
-);
+declare module "@adobe/react-spectrum" {
+  interface RouterConfig {
+    routerOptions: NavigateOptions;
+  }
+}
+
+const App = () => {
+  const navigate = useNavigate();
+  return (
+    <BrowserRouter>
+      <Provider theme={defaultTheme} router={{ navigate, useHref }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/upload" element={<UploadPage />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  );
+};
 
 createRoot(document.getElementById("app")!).render(<App />);
