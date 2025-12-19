9d95b5f571dde8a1b2a8e6781dec387e37a2bb20
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-12-19 10:31:56 +0530

    feat: created repository for role and user entities

---
+++ vimo_backend/src/main/java/com/avaneesh/vimo_backend/auth/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.avaneesh.vimo_backend.auth.repositories;
+
+import com.avaneesh.vimo_backend.auth.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+}
