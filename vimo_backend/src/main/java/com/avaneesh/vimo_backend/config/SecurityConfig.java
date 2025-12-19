5bb5f9af56148f57725fe66b6a9c65bffdbf65cc
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-12-19 14:28:49 +0530

    feat: configured spring security in auth module

---
+++ vimo_backend/src/main/java/com/avaneesh/vimo_backend/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.avaneesh.vimo_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v1/auth/register",
+                                "/v1/auth/login",
+                                "/actuator/**",
+                                "/v3/api-docs/**",
+                                "/docs",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .logout(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
