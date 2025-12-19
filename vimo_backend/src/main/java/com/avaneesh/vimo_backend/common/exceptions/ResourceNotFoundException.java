cbb1de5499ba6890c2b5884921d960e56569d089
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-12-19 10:29:00 +0530

    feat: created ResourceNotFoundException class in common module

---
+++ vimo_backend/src/main/java/com/avaneesh/vimo_backend/common/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.avaneesh.vimo_backend.common.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException() {
+        super("Resource Not Found");
+    }
+}
