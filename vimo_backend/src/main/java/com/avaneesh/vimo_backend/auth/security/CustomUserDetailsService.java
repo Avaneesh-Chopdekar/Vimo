68b921428b077602754a59438cd5a5c14df2754b
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-12-19 14:29:17 +0530

    created CustomUserDetailsService class with loadUserByUsername() method

---
+++ vimo_backend/src/main/java/com/avaneesh/vimo_backend/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.avaneesh.vimo_backend.auth.security;
+
+import com.avaneesh.vimo_backend.auth.entities.User;
+import com.avaneesh.vimo_backend.auth.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+    }
+}
