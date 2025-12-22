9c65dfb913d87b340ee4db6ee05e4c6eb1a88f7d
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-12-23 00:16:42 +0530

    feat: handled exception, sessionManagement and added authenticationManager bean in SecurityConfig

--- vimo_backend/src/main/java/com/avaneesh/vimo_backend/config/SecurityConfig.java
+++ vimo_backend/src/main/java/com/avaneesh/vimo_backend/config/SecurityConfig.java
@@ -1,27 +1,42 @@
 package com.avaneesh.vimo_backend.config;
 
+import com.avaneesh.vimo_backend.auth.security.JwtAuthenticationFilter;
+import com.avaneesh.vimo_backend.common.dtos.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v1/auth/register",
+                        .requestMatchers(
+                                "/v1/auth/register",
                                 "/v1/auth/login",
+                                "/v1/auth/refresh",
                                 "/actuator/**",
                                 "/v3/api-docs/**",
                                 "/docs",
@@ -31,11 +46,32 @@ public class SecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults())
                 .logout(AbstractHttpConfigurer::disable)
+                .exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, e) -> {
+                    //error message
+//                    e.printStackTrace();
+                    response.setStatus(401);
+                    response.setContentType("application/json");
+                    String message = e.getMessage();
+                    String error = (String) request.getAttribute("error");
+                    if (error != null) {
+                        message = error;
+                    }
+//                    Map<String, Object> errorMap = Map.of("message", message, "statusCode",404);
+                    var apiError = new ErrorResponse("Unauthorized Access", HttpStatus.UNAUTHORIZED.value(),  message, System.currentTimeMillis());
+                    var objectMapper = new ObjectMapper();
+                    response.getWriter().write(objectMapper.writeValueAsString(apiError));
+                }))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
